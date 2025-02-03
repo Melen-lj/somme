066f6fc3b7ecc5d58eddb6008fbd0a2b95d054f7
Melen-lj <114390137+Melen-lj@users.noreply.github.com>
2025-02-03 10:32:06 +0100

    Add files via upload

---
+++ UDP/serveur_udp_somme.c
@@ -0,0 +1,21 @@
+#include "udp.h"
+#include "erreur.h"
+#include "nombre.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+
+int main(int argc, char** argv) {
+    traiter_commande(argc == 2, argv[0], "<port>\nmauvais nombre d'arguments");
+    traiter_commande(est_nombre(argv[1]), argv[0], "<port>\n<port> doit être un nombre");
+
+    int port = atoi(argv[1]);
+
+    traiter_commande(port > 1024, argv[0], "<port>\n<port> est un port non réservé");
+    
+
+   
+    exit(0);
+}
